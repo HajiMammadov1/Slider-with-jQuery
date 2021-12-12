0acac3749c284a3329340e6cc27c3fa3ff131215
HajiMammadov1 <hajiam@code.edu.az>
2021-12-12 11:44:03 -0800

    update1.1

--- SliderJQuery/assets/js/script.js
+++ SliderJQuery/assets/js/script.js
@@ -3,18 +3,18 @@ $(function () {
     //Next button click shows next image and checking the image is the last element of the div,
     //if so then returns to the first image
 
-    $(".next").on("click", function () {
-        let currentImg = $(".active");
-        let nextImg = currentImg.next();
-        let firstImg = $("div img").first();
+    $(".nextBtn").on("click", function () {
+        let currentImgSelect = $(".active");
+        let nextImgSelect = currentImgSelect.next();
+        let firstImgSelect = $("div img").first();
        
-        if (nextImg.length) {
-            currentImg.removeClass("active").css("z-index", -10);
-            nextImg.addClass("active").css("z-index", 10);
+        if (nextImgSelect.length) {
+            currentImgSelect.removeClass("active").css("z-index", -10);
+            nextImgSelect.addClass("active").css("z-index", 10);
 
         }else if($("div img").last()) {
-            currentImg.removeClass("active").css("z-index", -10);
-            firstImg.addClass("active").css("z-index", 10);
+            currentImgSelect.removeClass("active").css("z-index", -10);
+            firstImgSelect.addClass("active").css("z-index", 10);
 
         }
     });
@@ -22,15 +22,15 @@ $(function () {
     //Previous button click shows previous image and checking the image is the first element of the div,
     //if so then returns to the last image
 
-    $(".prev").on("click", function () {
-        let currentImg = $(".active");
-        let prevImg = currentImg.prev();
+    $(".prevBtn").on("click", function () {
+        let currentImgSelect = $(".active");
+        let prevImgSelect = currentImgSelect.prev();
 
-        if (prevImg.length) {
-            currentImg.removeClass("active").css("z-index", -10);
-            prevImg.addClass("active").css("z-index", 10);
+        if (prevImgSelect.length) {
+            currentImgSelect.removeClass("active").css("z-index", -10);
+            prevImgSelect.addClass("active").css("z-index", 10);
         }else if($("div img").first()) {
-            currentImg.removeClass("active").css("z-index", -10);
+            currentImgSelect.removeClass("active").css("z-index", -10);
             $("div img").last().addClass("active").css("z-index", 10);
 
         }
